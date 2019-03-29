cb8a11b8b933d0163c69cada900e2c17e756a838
Alpaca53382 <notnat.tlikehis@gmail.com>
2019-03-28 17:52:05 -0700

    pfp

--- bot.js
+++ bot.js
@@ -143,7 +143,7 @@ function checkItalics(msg) { // Function to be fired if a message is valid for i
                     });
                 }, 5000);
             } else if (imgPoke == "furry") {
-                msg.channel.send("you");
+                msg.channel.send('',{file: msg.author.user.displayAvatarURL});
             } else if (imgPoke == "dab") {
                 msg.channel.send('',{file: {attachment: "./kadabra.png"}});
             } else {
